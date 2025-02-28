242f9c635e2b30ddf859414c7539cc8791d12dd5
Julie_Fromageat <j.fromageat@unistra.fr>
2025-02-28 15:05:10 +0100

    integrate GameProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+// src/App.jsx
+import React from 'react';
+import { GameProvider } from './contexts/GameContext';
+import TitleScreen from './screens/TitleScreen';
+import DebugButton from './components/DebugButton';
+import './assets/styles/index.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// Composant principal simplifié qui ne dépend plus de isGameStarted
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <GameProvider>
+      <div className="app">
+        <TitleScreen />
+        <DebugButton />
+      </div>
+    </GameProvider>
+  );
+};
 
-export default App
+export default App;
